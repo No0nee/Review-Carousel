012366d5bae2537a5114fe1cf3a9308534645caf
david <hall.davidwv@gmail.com>
2023-09-01 16:10:14 +0800

    Add person 2 img and new array
    Added person-2.png file in folder, created an array for other persons for the review, and added the data for the second person.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Goju Satoru",
+    role: "Jujutsu Sorcerer",
+    info: "One of the main protagonists of the Jujutsu Kaisen series. Widely recognized as the strongest in the world."
+  }
+]
+
 // Selecting Items
 
 const img = document.getElementById("person-img");
 const name = document.getElementById("name");
-const hobby = document.getElementById("hobby");
+const role = document.getElementById("role");
 const info = document.getElementById("info");
 
 const prevBtn = document.querySelector(".prev-btn");
