de1ea652b342c6b744177ff7c937b784fbd3cd70
david <hall.davidwv@gmail.com>
2023-09-01 15:51:28 +0800

    Add selecting items
    Added getElementById and querySelector functions in app.js to match the elements for JS

---
+++ app.js
@@ -0,0 +1,9 @@
+// Selecting Items
+
+const img = document.getElementById("person-img");
+const name = document.getElementById("name");
+const hobby = document.getElementById("hobby");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
